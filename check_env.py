2e588e62519b06039217ceaf395fb2dd56990012
anshul <anshulsinghkamboj@gmail.com>
2025-10-29 22:16:24 +0530

    initial uv setup

---
+++ check_env.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+import sklearn
+print("Environment check successful!")
+print("numpy:", np.__version__)
+print("pandas:", pd.__version__)
+print("scikit-learn:", sklearn.__version__)
