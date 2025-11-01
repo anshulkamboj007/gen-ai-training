907f1c5dd65b2750882ff7468f48033272b0500f
anshul <anshulsinghkamboj@gmail.com>
2025-11-01 06:33:20 +0530

    from home

--- week_1/day_1/data_loader.py
+++ week_1/day_1/data_loader.py
@@ -1,9 +1,24 @@
 import pandas as pd
+import logging
+import os
+
+logging.basicConfig(level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(message)s")
+
 
 def load_data(file_path):
-    df1=pd.read_csv(file_path)
-    df1=df1.drop(columns=['Unnamed: 0'])
-    print('data loaded successfully')
+    if not os.path.exists(file_path):
+        logging.error("File not found: %s", file_path)
+        raise FileNotFoundError(f"{file_path} not found.")
+    try:
+        logging.debug("reading file path")
+        df1=pd.read_csv(file_path)
+        logging.debug("cleaning ")
+        df1=df1.drop(columns=['Unnamed: 0'])
+        logging.debug("loaded data...")
+    except Exception as e:
+        logging.error("Failed to load data: %s", e)
     return df1
 
-load_data(r'C:\Users\NODRS12290\Desktop\ai ml training\week_1\day_1\Salary_dataset.csv')
+if __name__ == "__main__":
+    #for testing only
+    load_data(r'./week_1/day_1/Salary_dataset.csv')
