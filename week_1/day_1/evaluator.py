58701574e50548679dda6596661613103e680f76
anshulsinghkamboj <anshulsinghkamboj@gmail.com>
2025-11-01 11:36:25 +0530

    Delete week_1/day_1 directory

--- week_1/day_1/evaluator.py
+++
@@ -1,8 +0,0 @@
-from sklearn.metrics import mean_squared_error, r2_score
-
-def evaluate_model(model, X, y):
-    y_pred = model.predict(X)
-    return {
-        "mse": mean_squared_error(y, y_pred),
-        "r2": r2_score(y, y_pred)
-    }
