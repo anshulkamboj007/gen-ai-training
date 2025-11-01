0219bf2b3c7fd53cf9670612b51fb08d7fb7a097
anshul kamboj <anshulsinghkamboj@gmail.com>
2025-11-01 14:45:28 +0530

    r1

--- week_1/day_1/data_loader.py
+++ week_1/day_1/data_loader.py
@@ -1,22 +1,21 @@
 import pandas as pd
-import logging
 import os
-
-logging.basicConfig(level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(message)s")
+from logger_config import get_logger
+logger = get_logger(__name__)
 
 
 def load_data(file_path):
     if not os.path.exists(file_path):
-        logging.error("File not found: %s", file_path)
+        logger.error("File not found: %s", file_path)
         raise FileNotFoundError(f"{file_path} not found.")
     try:
-        logging.debug("reading file path")
+        logger.debug("reading file path")
         df1=pd.read_csv(file_path)
-        logging.debug("cleaning ")
+        logger.info("cleaning ")
         df1=df1.drop(columns=['Unnamed: 0'])
-        logging.debug("loaded data...")
+        logger.info("loaded data...")
     except Exception as e:
-        logging.error("Failed to load data: %s", e)
+        logger.error("Failed to load data: %s", e)
     return df1
 
 if __name__ == "__main__":
--- week_1/day_1/model_trainer.py
+++ week_1/day_1/model_trainer.py
@@ -1,15 +1,21 @@
 from sklearn.linear_model import LinearRegression
-import logging
-logging.basicConfig(level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(message)s")
+from sklearn.metrics import r2_score
+import mlflow
 
-def train_model(X,y):
-    try:
-        logging.debug("creating model")
-        model=LinearRegression()
-        model.fit(X,y)
-        logging.debug("model fit done")
-    except Exception as e:
-        logging.error("Failed to create / load model: %s", e)
-    return model
+def train_model(X_train, y_train, X_test, y_test):
+    with mlflow.start_run():
+        model = LinearRegression()
+        model.fit(X_train, y_train)
+
+        preds = model.predict(X_test)
+        r2 = r2_score(y_test, preds)
+
+        # Log metrics and parameters
+        mlflow.log_param("model_type", "scratch")
+        mlflow.log_metric("r2_score", r2)
+        mlflow.log_artifact("logs/train.log")
+
+        mlflow.sklearn.log_model(model, "model")
+
     
     
